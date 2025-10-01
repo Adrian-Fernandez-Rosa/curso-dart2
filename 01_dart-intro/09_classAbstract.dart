db8bdfe046f4f6389a3a6c04ad0d8fd39d9fc683
Adrian-Fernandez-Rosa <adrianfr2011@gmail.com>
2025-10-02 00:31:35 +0200

    class abstract

--- 01_dart-intro/09_classAbstract.dart
+++ 01_dart-intro/09_classAbstract.dart
@@ -1,4 +1,12 @@
-void main() {}
+void main() {
+  Animal perro1 = new Perro(peso: 40, tipo: TipoAnimal.Perro, energia: 44);
+
+  print(perro1.toString());
+  perro1.consumeEnergia(20);
+
+  print('\n');
+  print(perro1.toString());
+}
 
 enum TipoAnimal { Perro, Gato, Ave }
 
@@ -7,11 +15,27 @@ abstract class Animal {
   TipoAnimal tipo; // Perro, Gato, Ave
   double energia;
 
-  Animal({
-    required this.peso,
-    required this.tipo,
-    required this.energia
-  });
-  
+  Animal({required this.peso, required this.tipo, required this.energia});
+
   void consumeEnergia(double cantidad);
+}
+
+class Perro extends Animal {
+  Perro({required super.peso, required super.tipo, required super.energia});
+  
+
+
+
+  void consumeEnergia(double cantidad) {
+    energia -= cantidad;
+  }
+
+  @override
+  String toString() {
+    return ("""
+    Tipo: $tipo
+    Peso: $peso
+    Energia: $energia
+    """);
+  }
 }
